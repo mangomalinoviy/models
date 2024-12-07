334f266e9a8f7ca9d31e83f8751d15cf58635879
Irisken <ggfhtder@gmail.com>
2024-12-07 18:55:41 +1000

    Auto-committed on 07.12.2024

--- push.bat
+++ push.bat
@@ -1,4 +1,10 @@
-start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login
-git add .
-git commit -m enti
-git push
+@echo off
+
+set GIT_PATH="C:\Program Files\Git\bin\git.exe"
+set BRANCH = "models"
+
+%GIT_PATH% add .
+%GIT_PATH% commit -am "Auto-committed on %date%"
+%GIT_PATH% pull %BRANCH%
+%GIT_PATH% push %BRANCH%
+
