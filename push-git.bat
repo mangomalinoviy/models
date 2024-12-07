ef6667dc6fdfd29ceaedc4bfad497bc69b845d2d
Irisken <ggfhtder@gmail.com>
2024-12-07 19:25:32 +1000

    Auto-committed on 07.12.2024

--- push-git.bat
+++ push-git.bat
@@ -1,10 +1,11 @@
 @echo off
-
+cd "I:\godot projects\demo3d\gitmodels"
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "main"
+
 
 %GIT_PATH% add .
 %GIT_PATH% commit -am "Auto-committed on %date%"
 %GIT_PATH% pull %BRANCH%
 %GIT_PATH% push %BRANCH%
-
+pause
